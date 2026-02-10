174654d18be0158a3f941bc04d579ecc447bd79c
alamaeng <alamaeng@gmail.com>
2026-02-10 11:46:49 +0900

    Fix: Resize problem images to 50% width

--- src/components/MathRenderer.tsx
+++ src/components/MathRenderer.tsx
@@ -19,7 +19,7 @@ export default function MathRenderer({ content, className }: MathRendererProps &
                 components={{
                     // Custom components if needed, e.g. images
                     img: ({ node, ...props }) => (
-                        <img {...props} style={{ maxWidth: '100%', height: 'auto' }} alt={props.alt || 'image'} />
+                        <img {...props} style={{ width: '50%', height: 'auto' }} alt={props.alt || 'image'} />
                     ),
                     p: ({ node, ...props }) => (
                         <p {...props} className="mb-6 last:mb-0 text-black dark:text-white" />
